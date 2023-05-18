3c5f349a7b61c88a04b673b35fd2da0e4c7cdd62
siph <dawkins.chris.dev@gmail.com>
2023-05-18 17:50:27 -0600

    leader key and quick save/exit

---
+++ config/core.nix
@@ -0,0 +1,13 @@
+{
+  globals = {
+    mapleader = " ";
+  };
+  maps = {
+    normal."<leader>q" = {
+      action = "<cmd>qa<CR>";
+    };
+    normal."<leader>w" = {
+      action = "<cmd>w<CR>";
+    };
+  };
+}
--- config/default.nix
+++ config/default.nix
@@ -2,6 +2,7 @@
   # Import all your configuration modules here
   imports = [
     ./bufferline.nix
+    ./core.nix
     ./gruvbox.nix
   ];
 }
