8fb5645dca88c7daa621befa868ef91bb7a93af8
siph <dawkins.chris.dev@gmail.com>
2023-05-18 20:43:30 -0600

    lualine

--- config/default.nix
+++ config/default.nix
@@ -5,6 +5,7 @@
     ./core.nix
     ./gruvbox.nix
     ./lsp.nix
+    ./lualine.nix
     ./nvim-cmp.nix
     ./telescope.nix
     ./treesitter.nix
---
+++ config/lualine.nix
@@ -0,0 +1,17 @@
+{
+  plugins.lualine = {
+    enable = true;
+    theme = "gruvbox";
+    componentSeparators = {
+      left = "";
+      right = "";
+    };
+    sectionSeparators = {
+      left = "";
+      right = "";
+    };
+    sections = {
+      lualine_c = [ "filename" ];
+    };
+  };
+}
