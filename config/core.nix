ad7bc2c0f4f58a2d3b484da132fac92dc3f9b8ee
siph <dawkins.chris.dev@gmail.com>
2023-05-20 22:13:21 -0600

    pane navigation

--- config/core.nix
+++ config/core.nix
@@ -3,11 +3,26 @@
     mapleader = " ";
   };
   maps = {
-    normal."<leader>q" = {
-      action = "<cmd>qa<CR>";
-    };
-    normal."<leader>w" = {
-      action = "<cmd>w<CR>";
+    normal = {
+      "<leader>q" = {
+        action = "<cmd>qa<CR>";
+      };
+      "<leader>w" = {
+        action = "<cmd>w<CR>";
+      };
+
+      "<C-h>" = {
+        action = "<C-w>h";
+      };
+      "<C-j>" = {
+        action = "<C-w>j";
+      };
+      "<C-k>" = {
+        action = "<C-w>k";
+      };
+      "<C-l>" = {
+        action = "<C-w>l";
+      };
     };
   };
 }
