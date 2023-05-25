9d45fc07b50967704cc2fe1d78728a483984a867
siph <dawkins.chris.dev@gmail.com>
2023-05-24 19:27:48 -0600

    transparent background

--- config/gruvbox.nix
+++ config/gruvbox.nix
@@ -5,7 +5,19 @@
     trueColor = true;
   };
 
-  # Background transparency until I can figure out `autoCmd` and `autoGroup` options
+  highlight = {
+    Normal = {
+      bg = "none";
+    };
+    NormalFloat = {
+      bg = "none";
+    };
+    NormalNC = {
+      bg = "none";
+    };
+  };
+
+  # Background transparency until I can figure why bufferline isn't fully transparent.
   extraConfigVim = ''
       augroup user_colors
         autocmd!
