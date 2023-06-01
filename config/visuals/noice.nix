c751647d145150cd7bec4ba4feda7296971211b7
siph <dawkins.chris.dev@gmail.com>
2023-05-31 23:13:21 -0600

    message keybinds

--- config/visuals/noice.nix
+++ config/visuals/noice.nix
@@ -5,4 +5,14 @@
       notify.enabled = true;
     };
   };
+  maps.normal = {
+    "<leader>c" = {
+      desc = "Clear Messages";
+      action = "<cmd>lua require('noice').cmd('dismiss')<CR>";
+    };
+    "<leader>fm" = {
+      desc = "Find Messages";
+      action = "<cmd>lua require('noice').cmd('telescope')<CR>";
+    };
+  };
 }
