1112b71cae8c8aa361d9cbcc9f379648cc58072f
siph <dawkins.chris.dev@gmail.com>
2023-06-08 16:58:42 -0600

    extra noice handling

--- config/visuals/noice.nix
+++ config/visuals/noice.nix
@@ -3,6 +3,11 @@
     noice = {
       enable = true;
       notify.enabled = true;
+      lsp.override = {
+        "vim.lsp.util.convert_input_to_markdown_lines" = true;
+        "vim.lsp.util.stylize_markdown" = true;
+        "cmp.entry.get_documentation" = true;
+      };
     };
   };
   maps.normal = {
