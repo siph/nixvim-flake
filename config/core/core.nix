b691064d092641e007bf8d4e2af2af86f707196f
siph <dawkins.chris.dev@gmail.com>
2023-06-12 15:54:31 -0600

    better options
    replace some stuff with the nix style configs.

--- config/core/core.nix
+++ config/core/core.nix
@@ -1,63 +1,70 @@
 {
-  globals = {
-    mapleader = " ";
-  };
-  maps = {
-    normal = {
-      "<leader>q" = {
-        desc = "Quit NeoVim";
-        action = "<cmd>qa<CR>";
-      };
-      "<leader>w" = {
-        desc = "Save Current Buffer";
-        action = "<cmd>w<CR>";
-      };
+  config = {
+    globals = {
+      mapleader = " ";
+      loaded_ruby_provider = 0;
+      loaded_perl_provider = 0;
+      loaded_python_provider = 0;
+    };
+    maps = {
+      normal = {
+        "<leader>q" = {
+          desc = "Quit NeoVim";
+          action = "<cmd>qa<CR>";
+        };
+        "<leader>w" = {
+          desc = "Save Current Buffer";
+          action = "<cmd>w<CR>";
+        };
 
-      "<C-h>" = {
-        action = "<C-w>h";
-      };
-      "<C-j>" = {
-        action = "<C-w>j";
-      };
-      "<C-k>" = {
-        action = "<C-w>k";
-      };
-      "<C-l>" = {
-        action = "<C-w>l";
+        "<C-h>" = {
+          action = "<C-w>h";
+        };
+        "<C-j>" = {
+          action = "<C-w>j";
+        };
+        "<C-k>" = {
+          action = "<C-w>k";
+        };
+        "<C-l>" = {
+          action = "<C-w>l";
+        };
       };
     };
-  };
-  clipboard = {
-    register = "unnamedplus";
-    providers = {
-      xsel.enable = true;
-      wl-copy.enable = true;
+    clipboard = {
+      register = "unnamedplus";
+      providers = {
+        xsel.enable = true;
+        wl-copy.enable = true;
+      };
     };
+    options = {
+        autoindent = true;
+        expandtab = true;
+        hidden = true;
+        incsearch = true;
+        number = true;
+        relativenumber = true;
+        scrolloff = 15;
+        shiftwidth = 4;
+        signcolumn = "yes";
+        softtabstop = 4;
+        spell = true;
+        splitbelow = true;
+        splitright = true;
+        tabstop = 4;
+        termguicolors = true;
+        updatetime = 100;
+    };
+    extraConfigVim = ''
+
+        " Highlight on yank
+        augroup highlight_yank
+            autocmd!
+            au TextYankPost * silent! lua vim.highlight.on_yank{higroup="Search", timeout=200}
+        augroup END
+
+        autocmd filetype nix setlocal tabstop=2 shiftwidth=2 softtabstop=2
+    '';
   };
-  extraConfigVim = ''
-      set tabstop=4
-      set shiftwidth=4
-      set softtabstop=4
-      set expandtab
-      set hidden
-      set scrolloff=15
-      set spell
-      set splitbelow
-      set splitright
-      set signcolumn=yes
-      set autoindent
-      set noerrorbells
-      set novisualbell
-      set relativenumber
-      set number
-      set incsearch
-
-      " Highlight on yank
-      augroup highlight_yank
-          autocmd!
-          au TextYankPost * silent! lua vim.highlight.on_yank{higroup="Search", timeout=200}
-      augroup END
-
-      autocmd filetype nix setlocal tabstop=2 shiftwidth=2 softtabstop=2
-  '';
 }
