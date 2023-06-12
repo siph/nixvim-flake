24c7f990bdeb620622d35249995efd64730c346e
siph <dawkins.chris.dev@gmail.com>
2023-06-12 14:58:22 -0600

    fix snippets

--- config/lsp/nvim-cmp.nix
+++ config/lsp/nvim-cmp.nix
@@ -5,51 +5,27 @@
       enable = true;
       sources = [
         { name = "buffer"; }
+        { name = "luasnip"; }
+        { name = "nvim_lsp"; }
         { name = "path"; }
         { name = "treesitter"; }
-        { name = "nvim_lsp"; }
       ];
-    mapping = {
-      "<C-b>" = "cmp.mapping.scroll_docs(-4)";
-      "<C-f>" = "cmp.mapping.scroll_docs(4)";
-      "<C-Space>" = ''        cmp.mapping.complete({
-                    config = {
-                      sources = {
-                        { name = "buffer" },
-                        { name = "path" },
-                        { name = "treesitter" },
-                        { name = "nvim_lsp" },
-                      }
-                    }
-                  })'';
-      "<C-e>" = "cmp.mapping.abort()";
-      "<CR>" = "cmp.mapping.confirm({ select = true })";
-      "<Tab>" = {
-        action = ''
-          function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            else
-              fallback()
-            end
-          end
-        '';
-        modes = ["i" "s"];
+      mapping = {
+        "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+        "<C-f>" = "cmp.mapping.scroll_docs(4)";
+        "<C-Space>" = "cmp.mapping.complete()";
+        "<C-e>" = "cmp.mapping.abort()";
+        "<CR>" = "cmp.mapping.confirm({ select = true })";
+        "<Tab>" = {
+          action = "cmp.mapping.select_next_item()";
+          modes = ["i" "s"];
+        };
+        "<S-Tab>" = {
+          action = "cmp.mapping.select_prev_item()";
+          modes = ["i" "s"];
+        };
       };
-      "<S-Tab>" = {
-        action = ''
-          function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            else
-              fallback()
-            end
-          end
-        '';
-        modes = ["i" "s"];
-      };
-    };
-
+      snippet.expand = "luasnip";
     };
     cmp-buffer.enable = true;
     cmp-path.enable = true;
