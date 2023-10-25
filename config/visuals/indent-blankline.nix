a3f90d1f410bc7457d606c6cc1a04929e6c5946c
siph <dawkins.chris.dev@gmail.com>
2023-10-25 16:22:08 -0600

    update indent-blankline options

--- config/visuals/indent-blankline.nix
+++ config/visuals/indent-blankline.nix
@@ -1,9 +1,8 @@
 {
   plugins.indent-blankline = {
     enable = true;
-    char = "";
-    useTreesitter = true;
-    showCurrentContext = false;
-    showEndOfLine = true;
+    indent = {
+      char = "";
+    };
   };
 }
