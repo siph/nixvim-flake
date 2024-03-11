90cf9ba810ba1b1b26d6135fe77420f7d5c55e99
siph <dawkins.chris.dev@gmail.com>
2024-03-10 19:50:15 -0600

    refactor transparency

--- config/visuals/theme/gruvbox.nix
+++ config/visuals/theme/gruvbox.nix
@@ -1,28 +1,43 @@
 {
   colorschemes.gruvbox = {
     enable = true;
+
     settings = {
       true_color = true;
       contrast_dark = "hard";
     };
   };
+
   highlight = {
+    BufferlineSeparator.fg = "#ebdbb2";
+    BufferlineTabSeparator.fg = "#ebdbb2";
+    BufferLineTabClose.fg = "#ebdbb2";
+    BufferLineSeparatorSelected.fg = "#ebdbb2";
+    BufferLineSeparatorVisible.fg = "#ebdbb2";
+    BufferLineTabSeparatorVisible.fg = "#ebdbb2";
+    BufferLineGroupLabel.fg = "#ebdbb2";
+    NvimTreeWinSeparator.fg = "#ebdbb2";
+
     GruvboxGreenSign = {
       bg = "none";
       fg = "#b8bb26";
     };
+
     GruvboxRedSign = {
       bg = "none";
       fg = "#fb4934";
     };
+
     GruvboxBlueSign = {
       bg = "none";
       fg = "#83a598";
     };
+
     GruvboxYellowSign = {
       bg = "none";
       fg = "#fabd2f";
     };
+
     GruvboxAquaSign = {
       bg = "none";
       fg = "#8ec07c";
--- config/visuals/theme/transparent-background.nix
+++ config/visuals/theme/transparent-background.nix
@@ -1,33 +1,43 @@
-{
-  highlight = {
-    Normal = {
-      bg = "none";
-    };
-    NormalFloat = {
-      bg = "none";
-    };
-    NormalNC = {
-      bg = "none";
-    };
-    WinSeparator = {
-      bg = "none";
-    };
-    WinBar = {
-      bg = "none";
-    };
-    SignColumn = {
-      bg = "none";
-    };
-    StatusLine = {
-      bg = "none";
-    };
-    Pmenu = {
-      bg = "none";
-    };
-    CursorLine = {
-      bg = "none";
-    };
-  };
+{pkgs, ...}: {
+  extraPlugins = with pkgs; [vimPlugins.transparent-nvim];
+
+  extraConfigLua = ''
+    vim.g.transparent_groups = vim.list_extend(
+      vim.g.transparent_groups or {},
+      vim.tbl_map(function(v)
+        return v.hl_group
+      end, vim.tbl_values(require('bufferline.config').highlights))
+    )
+
+    vim.g.transparent_groups = vim.list_extend(
+      vim.g.transparent_groups or {},
+      {
+        "NormalFloat",
+        "NvimTreeNormal",
+        "Folded",
+        "FoldColumn",
+        "TablineFill",
+        "ColorColumn",
+        "NormalNC",
+        "WinSeparator",
+        "WinBar",
+        "SignColumn",
+        "StatusLine",
+        "Pmenu",
+        "CursorLine",
+        "WildMenu",
+        "GruvboxBlueSign",
+        "GruvboxYellowSign",
+        "GruvboxAquaSign",
+        "GruvboxGreenSign",
+        "GruvboxRedSign",
+        "GruvboxPurpleSign",
+        "GruvboxOrangeSign",
+        "BufferLineDevIconNixSelected",
+        "BufferLineDevIconNixInactive",
+      }
+    )
+  '';
 
   extraConfigVim = ''
     augroup user_colors
